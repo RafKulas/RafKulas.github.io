6eee413188dcd6496c68b1432d3eb6e235bfaab4
RafKulas <leormas1@gmail.com>
2020-01-27 13:10:51 +0100

    Small changes in input

---
+++ customQuiz.js
@@ -0,0 +1,63 @@
+var type = new Array(18);
+
+var offensiveType;
+var defensiveType;
+
+var tries = 0;
+var score = 0;
+var questionsNO = 10;
+
+var multipliers = new Array(18);
+for(i=0; i<18;i++) {
+    multipliers[i] = new Array(18);
+}
+
+function init() {
+    $.getJSON( "https://api.myjson.com/bins/ic400", function( json ) {
+        pokemonData = json;
+        for(i=0; i<18; ++i) {
+            type[i] = String(json.types[i]);
+            for(j=0; j<18; ++j) {
+                multipliers[i][j] = Number(json.TypeStrength[i][j]);
+            }
+        }
+        })
+        .done(function() {
+            $("#firstTime").css("visibility", "visible");
+            $("#firstTime").attr("onclick","start()");
+            $("#firstTime").attr("value","Start!");
+
+          });
+   }
+
+function start() {
+    offensiveType = Math.round(Math.random()*17);
+    defensiveType = Math.round(Math.random()*17);
+
+    console.log(type);
+
+    $("#Question").html("Pytanie "+(tries+1)+"/"+questionsNO+"<br>Jaki mnoznik ma atak typu "+type[offensiveType]+ " w pokemon'a typu "+type[defensiveType]+"?");
+    $("#afterStart").css("display", "block");
+    $("#firstTime").css("display", "none");
+}
+
+function check() {
+    var attack = $("input[name=multiplier]:checked", "#Answer").val();
+    if(multipliers[offensiveType][defensiveType]==attack) {
+        score++;
+    }
+    tries ++;
+    if(tries == questionsNO) {
+        alert("Twój wynik to: " + (score) + "/" + (questionsNO));
+        score = 0;
+        tries = 0;
+    }
+    start();
+    $("#default").prop("checked", true);
+}
+
+function main() {
+    init();
+}
+
+window.onload = main
